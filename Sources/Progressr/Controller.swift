89f69497972263e4920742a0162df1b217dc24d8
Drew R. Hood <drew@drewrhood.me>
2017-03-14 15:41:55 -0700

    Introduced Controller class to better segregate code.

---
+++ Sources/Progressr/Controller.swift
@@ -0,0 +1,86 @@
+//
+//  Controller.swift
+//  Progressr
+//
+//  Created by Drew Hood on 3/14/17.
+//
+//
+
+import Foundation
+import Kitura
+import PerfectLogger
+
+class Controller {
+    public let router = Router()
+    public var port: Int {
+        let servicePort = CONFIGURATION["service:port"] as? String ?? "8090"
+        return Int(servicePort)!
+    }
+    
+    // Init
+    init?() {
+        
+        do {
+            // Set up retriever
+            try PilotEdgeRetriever.sharedRetriever.start()
+            
+            // Load airports
+            try AirportDatabase.sharedDatabase.loadAirports()
+            
+        } catch PilotEdgeInterfaceError.retrievalError {
+            LogFile.critical("PE retrieval error!")
+            return nil
+        } catch AirportDatabaseError.nfdc(let message) {
+            LogFile.critical(message ?? "Unknown Airport database error!")
+            return nil
+        } catch let error {
+            LogFile.critical("Unknown error initializing Controller - \(error.localizedDescription)")
+            return nil
+        }
+        
+        // Register routes
+        self.registerRoutes()
+    }
+    
+    private func registerRoutes() {
+        router.get("status/:id", handler: getPeStatus)
+        router.get("airport/:code", handler: getAirport)
+        
+        LogFile.debug("Controller registered routes")
+    }
+    
+    // Route Handlers
+    public func getPeStatus(request: RouterRequest, response: RouterResponse, _: @escaping () -> Void) throws {
+        response.headers["Content-Type"] = "application/json; charset=utf-8"
+        
+        if let pid = request.parameters["id"] {
+            if let status = try PilotEdgeInterface.sharedStatus.status(Int(pid)!) {
+                // Convert to JSON.
+                try response.status(.OK).send(status.jsonString).end()
+            } else {
+                try response.status(.notFound).send("{\"error\":\"No pilot with id \(pid).\"}").end()
+            }
+        } else {
+            try response.status(.notAcceptable).send("{\"error\":\"No pilot id provided.\"}").end()
+        }
+    }
+    
+    public func getAirport(request: RouterRequest, response: RouterResponse, _: @escaping () -> Void) throws {
+        response.headers["Content-Type"] = "application/json; charset=utf-8"
+        
+        // Get code without K
+        if var faaCode = request.parameters["code"] {
+            if faaCode.characters.count > 3 {
+                faaCode.remove(at: faaCode.startIndex)
+            }
+            
+            if let airport = AirportDatabase.sharedDatabase[faaCode.uppercased()] {
+                try response.status(.OK).send(airport.jsonString).end()
+            } else {
+                try response.status(.notFound).send("{\"error\":\"No airport found with code \(faaCode).\"}").end()
+            }
+        } else {
+            try response.status(.notAcceptable).send("{\"error\":\"No airport code provided.\"}").end()
+        }
+    }
+}
--- Sources/Progressr/main.swift
+++ Sources/Progressr/main.swift
@@ -16,6 +16,8 @@ import PerfectLogger
         case ALRM   = 14
         case TERM   = 15
     }
+#else
+    import Darwin
 #endif
 
 // Load config
@@ -27,99 +29,42 @@ fileprivate let logFilePath = CONFIGURATION["service:logpath"] as? String ?? "/v
 LogFile.location = logFilePath
 LogFile.debug("Bootstrapped logging to file \(logFilePath)")
 
-// Create a new router
-let router = Router()
+////////////////////////////////
+// Set up controller and server
+if let controller = Controller() {
+    Kitura.addHTTPServer(onPort: controller.port, with: controller.router)
+} else {
+    LogFile.critical("Couldn't init Controller! Quitting!")
+    exit(1)
+}
 
-router.get("/pe/status/:id") {
-    request, response, next in
+#if os(Linux)
     
-    if let pid = request.parameters["id"] {
-        if let status = try PilotEdgeInterface.sharedStatus.status(Int(pid)!) {
-            // Convert to JSON.
-            response.send(status.jsonString)
-        } else {
-            response.status(.notFound)
-            response.send("{\"error\":\"No pilot with id \(pid).\"}")
-        }
-    } else {
-        response.status(.notAcceptable)
-        response.send("{\"error\":\"No pilot id provided.\"}")
+    typealias SigactionHandler = @convention(c)(Int32) -> Void
+    
+    func trap(signum:Signal, action: @escaping SigactionHandler) {
+        signal(signum.rawValue, action)
     }
-
-    next()
-}
-
-router.get("airport/:code") {
-    request, response, next in
-
-    // Get code without K
-    if var faaCode = request.parameters["code"] {
-        if faaCode.characters.count > 3 {
-            faaCode.remove(at: faaCode.startIndex)
-        }
+    
+    let termHandler: SigactionHandler = { signal in
+        LogFile.info("Received SIGTERM. Closing servers and shutting down.")
         
-        if let airport = AirportDatabase.sharedDatabase[faaCode] {
-            response.send(airport.jsonString)
-        } else {
-            response.status(.notFound).send("{\"error\":\"No airport found with code \(faaCode).\"}")
-        }
-    } else {
-        response.status(.notAcceptable).send("{\"error\":\"No airport code provided.\"}")
+        // Stop calling PE
+        PilotEdgeRetriever.sharedRetriever.stop()
+        
+        Kitura.stop()
+        
+        exit(0)
     }
-
-    next()
-}
-
-do {
-    // Set up retriever
-    try PilotEdgeRetriever.sharedRetriever.start()
     
-    // Load airports
-    try AirportDatabase.sharedDatabase.loadAirports() // We want to crash if this fails!
+    trap(signum: .TERM, action: termHandler)
+    trap(signum: .INT, action: termHandler)
     
-    // Add an HTTP server and connect it to the router
-    let servicePort = CONFIGURATION["service:port"] as? String ?? "8090"
-    LogFile.info("Starting service on port \(servicePort)")
+    Kitura.start()
     
-    Kitura.addHTTPServer(onPort: Int(servicePort)!, with: router)
+    select(0, nil, nil, nil, nil)
     
-    #if os(Linux)
-        
-        typealias SigactionHandler = @convention(c)(Int32) -> Void
-        
-        func trap(signum:Signal, action: @escaping SigactionHandler) {
-            signal(signum.rawValue, action)
-        }
-        
-        let termHandler: SigactionHandler = { signal in
-            LogFile.info("Received SIGTERM. Closing servers and shutting down.")
-            
-            // Stop calling PE
-            PilotEdgeRetriever.sharedRetriever.stop()
-            
-            Kitura.stop()
-            
-            exit(0)
-        }
-        
-        trap(signum: .TERM, action: termHandler)
-        trap(signum: .INT, action: termHandler)
-        
-        Kitura.start()
-        
-        select(0, nil, nil, nil, nil)
-        
-    #else
-        // Start the Kitura runloop (this call never returns)
-        Kitura.run()
-    #endif
-
-} catch PilotEdgeRetrieverError.networkFailure {
-    LogFile.critical("PE Network failure!")
-} catch PilotEdgeInterfaceError.retrievalError {
-    LogFile.critical("PE retrieval error!")
-} catch AirportDatabaseError.nfdc(let message) {
-    LogFile.critical(message ?? "Unknown Airport database error!")
-} catch {
-    LogFile.critical("Unknown error starting service!")
-}
+#else
+    // Start the Kitura runloop (this call never returns)
+    Kitura.run()
+#endif
